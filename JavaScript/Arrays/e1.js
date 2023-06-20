f18b250d90446d6389ab2db10fec66851f7ff43b
Nvijaysundar <80611602+Nvijaysundar@users.noreply.github.com>
2023-06-20 17:02:01 +0530

    Add files via upload

---
+++ JavaScript/Arrays/e1.js
@@ -0,0 +1,39 @@
+li = [10,20,30,40,10,60,70,10,90]
+/*console.log(li)
+console.log(li[0])
+console.log(li[4])
+console.log(li.length)
+
+for(var i=0;i<li.length;i++){
+    console.log(li[i])
+}
+console.log(li.indexOf(10))
+console.log(li.lastIndexOf(10))
+console.log(li.indexOf(100))
+console.log(li.includes(50))
+console.log(li.includes(30))
+
+console.log(li.push(100))
+console.log(li)
+console.log(li.pop())
+console.log(li)
+console.log(li.unshift(100))
+console.log(li)
+li = li.concat(111,114,147)
+console.log(li)
+console.log(li.join("*"))
+console.log(li.join("-"))*/
+
+console.log(li.slice(3,6))
+console.log(li.slice(-1))
+console.log(li.slice(-1,-5))
+console.log(li.slice(-6,-2))
+console.log(li.toString())
+console.log(li.valueOf())
+//console.log(li.fill(635))
+//console.log(li.splice(3,5))
+console.log(li)
+
+l = [1,2,3,4,5]
+console.log(l.splice(2,3,100,200,300))
+console.log(l)
